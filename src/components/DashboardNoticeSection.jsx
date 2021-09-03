393d4fb511a01e3cbb8a90f45badde63068b744d
Nxrth-x <devilsouls666@gmail.com>
2021-09-02 22:49:42 -0300

    Updated notice section Removed fragments and updated rendering validation

--- src/components/DashboardNoticeSection.jsx
+++ src/components/DashboardNoticeSection.jsx
@@ -1,5 +1,3 @@
-import { Fragment } from 'react'
-
 import useGraphQuery from '@hooks/useGraphQuery'
 import useToastError from '@hooks/useToastError'
 import { GET_NOTICES_QUERY } from '@services/graphql/queries'
@@ -16,24 +14,19 @@ export default function DashboardNoticeSection() {
 
   return (
     <div className="mb-8">
-      {noticesData ? (
-        <Fragment>
-          <h1 className="mb-4 text-4xl tracking-tighter font-bold">Avisos</h1>
-          <div>
-            {noticesData.map(notice => (
-              <DashboardNoticeSectionCard key={notice.id} notice={notice} />
-            ))}
-          </div>
-        </Fragment>
+      <h1 className="mb-4 text-4xl tracking-tighter font-bold">Avisos</h1>
+      {noticesData?.length > 0 ? (
+        <div>
+          {noticesData.map(notice => (
+            <DashboardNoticeSectionCard key={notice.id} notice={notice} />
+          ))}
+        </div>
       ) : (
-        <Fragment>
-          <h1 className="mb-4 text-4xl tracking-tighter font-bold">Avisos</h1>
-          <div className="p-12 border-2 text-center border-dashed rounded-lg">
-            <h1 className="text-2xl text-gray-400">
-              Nenhum aviso a ser mostrado! 🖖
-            </h1>
-          </div>
-        </Fragment>
+        <div className="p-12 border-2 text-center border-dashed rounded-lg">
+          <h1 className="text-2xl text-gray-400">
+            Nenhum aviso a ser mostrado! 🖖
+          </h1>
+        </div>
       )}
     </div>
   )
